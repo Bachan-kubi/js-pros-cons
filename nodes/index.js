6707c61e94a01d80cebece0cb5ca9bff9f1f9da0
Bachan-kubi <felixghagra@gmail.com>
2023-07-22 05:01:14 +0600

    nodes added

---
+++ nodes/index.js
@@ -0,0 +1,9 @@
+for (let li of document.querySelectorAll("li")) {
+  //    console.log(li.firstChild.data);
+//   console.log(li.firstChild.data);
+};
+
+// alert(document.body.lastChild.nodeType)
+// alert(document)
+console.log(document)
+console.log(document.constructor.name)
