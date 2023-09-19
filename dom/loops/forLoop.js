7dc4a37b36abcbb28c85852ee966efdaca7f8da1
Bachan-kubi <felixghagra@gmail.com>
2023-09-19 12:15:26 +0600

    loop added

---
+++ dom/loops/forLoop.js
@@ -0,0 +1,26 @@
+//01 for loop 
+// for(let i=1; i<=10; i++){
+//     // console.log('run '+ i+ ' steps' )
+// }
+
+// 02 do while loop
+// let i=0;
+// let msg = 'This is do while looop!';
+// let loops = document.getElementById('showLoop');
+// do{
+//     // console.log('inside')
+//     // loops.innerHTML += `
+//     // // <h2>${msg}</h2>`;
+//     // // i++;
+// } while(i<=20);
+// // console.log('outside');
+
+// 03 while loop
+// let n=10;
+// while(n<=9){
+//     console.log('while loop!');
+//     n = prompt('Enter number');
+//     n++;
+// }
+// console.log('outside loop!');
+
